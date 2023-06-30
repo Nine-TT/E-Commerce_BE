824d1feedaf843b9c9afdd7ffb9d3325d8bff02f
Chin <113930748+Nine-TT@users.noreply.github.com>
2023-06-30 09:47:41 +0700

    update model

--- model/order.go
+++ model/order.go
@@ -1,9 +1,7 @@
 package model
 
 type Order struct {
-	ID uint `json:"id" gorm:"primaryKey"`
-	//User      User    `gorm:"foreignkey:UserID"`
-	//Product   Product `gorm:"foreignkey:ProductID"`
+	ID        uint `json:"id" gorm:"primaryKey"`
 	UserID    uint `json:"user_id" form:"user_id" validate:"required"`
 	ProductID uint `json:"product_id" form:"product_id" validate:"required"`
 	Quantity  int  `json:"quantity" form:"quantity" validate:"required"`
--- model/user.go
+++ model/user.go
@@ -11,22 +11,27 @@ type User struct {
 	Address    string    `json:"address" validate:"required"`
 	Phone      string    `json:"phone" validate:"required"`
 	Email      string    `json:"email" validate:"required,email"`
-	PassWord   string    `json:"pass_word" validate:"required"`
-	GenderCode string    `json:"gender_code" validate:"required"`
-	RoleCode   string    `json:"role_code" gorm:"default:R2"`
+	Password   string    `json:"password" validate:"required"`
+	GenderCode string    `json:"gender_code" gorm:"varchar(50)" validate:"required"`
+	RoleCode   string    `json:"role_code" gorm:"Type:varchar(50); default:R3"`
 	CreatedAt  time.Time `json:"created_at"`
 	UpdatedAt  time.Time `json:"updated_at"`
 	state      bool      `json:"state"`
-	//Role       Role      `gorm:"references:ID"`
+
+	//belongs to
+	Role   Role   `gorm:"foreignkey:RoleCode"`
+	Gender Gender `gorm:"foreignkey:GenderCode"`
+
+	//has many (one-to-many)
+	Orders []Order `gorm:"foreignkey:UserID"`
 }
 
 type Role struct {
-	ID    uint   `gorm:"primaryKey"`
-	Key   string `gorm:"primaryKey" json:"key"`
+	Key   string `gorm:"primaryKey; Type:varchar(50)" json:"key"`
 	Value string `json:"value"`
 }
 
 type Gender struct {
-	Key   string `gorm:"primaryKey" json:"key"`
+	Key   string `gorm:"primaryKey; Type:varchar(50)" json:"key"`
 	Value string `json:"value"`
 }
